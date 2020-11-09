ebd1e9a8a1cf3805e65ada1a62e4bd8208ffdb1d
Mina Farrokhnia <m.farrokhnia1205@gmail.com>
2020-11-09 12:53:44 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "name_prefix" {
   description = "A prefix used for naming resources."
 }
-variable "env" {
-  description = "Environement name for creating codeArtifact on"
-}
+#variable "env" {
+#  description = "Environement name for creating codeArtifact on"
+#}
 variable "tags" {
   description = "A map of tags (key-value pairs) passed to resources."
   type        = map(string)
